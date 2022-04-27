9557334c802f71b9d939b74a0ad6e8d95a110b8f
Janae Perez <103264522+Janaebriannee@users.noreply.github.com>
2022-04-27 13:12:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function ChangeColor(){
+  let newcolor= makecolor
+  document.body.style.backgroundColor=genRandomColor();
+}
+
+function genRandomColor(){
+  let r=0,g=0,b=0;
+  r=Math.round(Math.random()*255);
+  g=Math.round(Math.random()*255);
+  b=Math.round(Math.random()*255);
+  console.log("rgb("+r+","+g+","+b+")")
+    return "rgb("+r+","+g+","+b+")";
+  
+}
